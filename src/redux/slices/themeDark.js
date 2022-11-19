072b36426d25b6e7742c3cdcafac64c55c076516
Rozyk90 <lukasz.rozycki90@gmail.com>
2022-11-19 18:09:08 +0100

    Update themeDark.js
    clear comments and deploy

--- src/redux/slices/themeDark.js
+++ src/redux/slices/themeDark.js
@@ -13,26 +13,3 @@ const themeDark = createSlice({
 export const { changeThemeDark } = themeDark.actions;
 
 export default themeDark.reducer;
-
-
-
-
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   themeDark: false,
-// };
-
-// const themeDark = createSlice({
-//   name: "themeDark",
-//   initialState,
-//   reducers: {
-//     changeThemeDark: (state) => {
-//       state.themeDark = !state.themeDark;
-//     },
-//   },
-// });
-
-// export const { changeThemeDark } = themeDark.actions;
-
-// export default themeDark.reducer;
